2756b791b9e6a035d2f9514fd92d9f36739504c5
liadino <60626130+liadino@users.noreply.github.com>
2021-01-28 19:25:06 -0700

    Sorter

---
+++ sorter.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class sorter  implements Comparator<Student>
+			 	{
+	public int compare(Student s1, Student s2) {
+		return s1.getLastName().compareTo(S2.getLastName());
+	}
+
+
+
+
+
+	}
